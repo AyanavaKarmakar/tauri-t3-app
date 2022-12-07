316dfb9cc2adea8bb7bd028a5694bee65f9b0d85
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-12-08 00:10:34 +0530

    done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ const Home: NextPage = () => {
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <button
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/20 p-5 text-white hover:bg-white/30"
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/20 p-10 text-white hover:bg-white/30"
               onClick={() => {
                 open("https://tauri.app");
               }}
@@ -34,15 +34,15 @@ const Home: NextPage = () => {
               </div>
             </button>
             <button
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/20 p-5 text-white hover:bg-white/30"
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/20 p-10 text-white hover:bg-white/30"
               onClick={() => {
                 open("https://create.t3.gg/en/introduction");
               }}
             >
               <h3 className="text-2xl font-bold">T3 Docs →</h3>
               <div className="text-justify text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
+                Learn more about Create T3 App — The best way to start a
+                full-stack, typesafe Next.js app.
               </div>
             </button>
           </div>
