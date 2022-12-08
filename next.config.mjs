26c84a0f088639e2db75e9783c0a90b8dc33018b
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-12-08 16:51:50 +0530

    test

--- next.config.mjs
+++ next.config.mjs
@@ -9,5 +9,10 @@
 const config = {
   reactStrictMode: true,
   swcMinify: true,
+  // Note: This experimental feature is required to use NextJS Image in SSG mode.
+  // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
+  images: {
+    unoptimized: true,
+  },
 };
 export default config;
